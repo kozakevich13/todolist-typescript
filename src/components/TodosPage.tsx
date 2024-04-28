65c98daf44b162154fdb9ae8e09dd1d6b347de50
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2024-04-28 18:37:00 +0300

    added type for todo elem

--- src/components/TodosPage.tsx
+++ src/components/TodosPage.tsx
@@ -1,25 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import AddTodoForm from "./AddTodoForm";
 import { deleteTodo } from "../features/todoSlice";
 import styles from "./TodosPage.module.scss";
+import { Todo } from "../types";
+import { RootState } from "../store";
 
 const TodosPage: React.FC = () => {
-  const todos = useSelector((state: any) =>
-    state.todos.todos.filter((todo: any) => !todo.deleted)
+  const todos = useSelector((state: RootState) =>
+    state.todos.todos.filter((todo: Todo) => !todo.deleted)
   );
-
   const dispatch = useDispatch();
+  const [deleting, setDeleting] = useState<number | null>(null);
 
   const handleDelete = (id: number) => {
-    dispatch(deleteTodo(id));
+    setDeleting(id);
+    setTimeout(() => {
+      dispatch(deleteTodo(id));
+      setDeleting(null);
+    }, 500);
   };
 
   return (
     <div className={styles.todosContainer}>
       <AddTodoForm />
-      {todos.map((todo: any) => (
-        <div key={todo.id} className={styles.todoItem}>
+      {todos.map((todo: Todo) => (
+        <div
+          key={todo.id}
+          className={`${styles.todoItem} ${
+            deleting === todo.id ? styles.todoItemExiting : ""
+          }`}
+        >
           {todo.text}
           <button
             onClick={() => handleDelete(todo.id)}
